6c3e1fc086e4fde060e2ff981f9ae884e08eaf3f
Amit Ramrakhyani <amit.rce21@sot.pdpu.ac.in>
2023-02-14 22:06:42 +0530

    Updated SingletonDesignPattern.cpp file

--- SingletonDesignPattern.cpp
+++ SingletonDesignPattern.cpp
@@ -1,27 +1,44 @@
 #include <iostream>
 using namespace std;
 
+int value=0;
+
 class Singleton {
     private:
         static Singleton* instance;
-        Singleton(int x){
-            cout << "Singleton " << x << " is being created." << endl;
-        }
+        Singleton(){}
     public:
-        static Singleton* getInstance(int a){
+        static Singleton* getInstance(){
             if(instance == NULL){
-                instance = new Singleton(a);
+                instance = new Singleton();
+                return instance;
             }
             return instance;
+        }
+        void Deposit(int amt){
+            value+=amt;
+        }
+        void Withdraw(int amt){
+            value-=amt;
         }
 };
 
 Singleton* Singleton::instance = NULL;
 
 int main(){
-    Singleton* s1 = Singleton::getInstance(2);
-    s1->getInstance(3);
-    Singleton* s2 = Singleton::getInstance(4);
-    Singleton* s3 = Singleton::getInstance(7);
+    Singleton* transaction1 = Singleton::getInstance();
+    transaction1->Deposit(1000);
+    cout << "Balance: " << value << endl;
+    cout << "ID: " << transaction1 << endl << endl;
+
+    Singleton* transaction2 = Singleton::getInstance();
+    transaction2->Withdraw(500);
+    cout << "Balance: " << value << endl;
+    cout << "ID: " << transaction2 << endl << endl;
+    
+    Singleton* transaction3 = Singleton::getInstance();
+    transaction3->Deposit(3700);
+    cout << "Balance: " << value << endl;
+    cout << "ID: " << transaction3 << endl << endl;
     return 0;
 }
